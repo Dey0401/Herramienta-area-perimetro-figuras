96c55c5f5023225ee22fd7efb81a912c772b492b
Dey0401 <deyssiyuliana@gmail.com>
2023-08-22 19:50:03 -0500

    Start declaring the abstract class

---
+++ libFigura2D/libFigura2D/clsFgura2D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libFigura2D
+{
+    public abstract class clsFgura2D
+    {
+        #region Atributes
+        protected float fltArea;
+        protected float fltPerimetro;
+        protected string strError;
+        #endregion
+
+        #region Properties 
+        public float Area
+        {
+            get { return fltArea; }
+        }
+
+        public float Perimetro
+        {
+            get { return fltPerimetro; }
+        }
+
+        public string Error
+        {
+            get { return strError; }
+        }
+        #endregion
+
+        #region Methots public
+        public abstract bool hallaArea();
+        public abstract bool hallaPerimetro();
+        
+        #endregion
+
+
+    }
+}
