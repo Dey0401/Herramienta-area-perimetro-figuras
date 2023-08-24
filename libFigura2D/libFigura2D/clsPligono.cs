3292734d551faafadb22ca250efc2cdef01dc6b9
Dey0401 <deyssiyuliana@gmail.com>
2023-08-24 17:55:29 -0500

    The abstract classes polygon and quadrilateral are added.

---
+++ libFigura2D/libFigura2D/clsPligono.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libFigura2D
+{
+    abstract class clsPligono : clsFgura2D
+    {
+        #region Atributes
+        protected float fltLado1;
+        #endregion
+        public float Lado1 
+        {
+            set { fltLado1 = value; }
+        }
+    }
+}
